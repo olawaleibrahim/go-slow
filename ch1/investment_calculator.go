24d0f956cc6721a3542b98ee6aff9bc57f4afbfa
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2024-09-14 12:23:35 +0100

    control structures

--- ch1/investment_calculator.go
+++ ch1/investment_calculator.go
@@ -8,34 +8,41 @@ import (
 const inflationRate = 2.5
 
 func main() {
-	var investmentAmount float64
-	var expectedReturnRate float64
-	var years float64
+	// var investmentAmount float64
+	// var expectedReturnRate float64
+	// var years float64
 
-	fmt.Print("Investment Amount: ")
-	fmt.Scan(&investmentAmount)
+	// fmt.Print("Investment Amount: ")
+	// fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected return rate: ")
-	fmt.Scan(&expectedReturnRate)
+	// fmt.Print("Expected return rate: ")
+	// fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
-	fmt.Scan(&years)
+	// fmt.Print("Years: ")
+	// fmt.Scan(&years)
 
-	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("Future value: %v \nFuture real valuue: %v", futureValue, futureRealValue)
-	fmt.Println(futureRealValue)
-	fmt.Println("Divisor result:", divisor(90, 34))
+	// fmt.Printf("Future value: %v \nFuture real valuue: %v", futureValue, futureRealValue)
+	// fmt.Println(futureRealValue)
+	fmt.Println("Divisor result:", divisor(90, 340))
 }
 
 func divisor(denominator float64, numerator float64) float64 {
 	if denominator == 0 {
 		return 0
+	} else if denominator > 1 {
+		if denominator > numerator {
+			return denominator / numerator
+		} else {
+			return numerator / denominator
+		}
+	} else {
+		return numerator / denominator
 	}
 
-	return numerator / denominator
 }
 
 func calculateFutureValues(
