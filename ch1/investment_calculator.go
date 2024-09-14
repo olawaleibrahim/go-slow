5d7375a523bd44baa6b3ef0074c291af464d8715
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2024-09-14 12:55:51 +0100

    infinite loop and break

--- ch1/investment_calculator.go
+++ ch1/investment_calculator.go
@@ -30,13 +30,15 @@ func main() {
 
 	var numerator float64
 	var denominator float64
-	for i := 0; i < 3; {
+	for {
 		fmt.Print("Enter numerator: ")
 		fmt.Scan(&numerator)
 		fmt.Print("Enter denominator: ")
 		fmt.Scan(&denominator)
 		fmt.Println("Divisor result:", divisor(denominator, numerator))
-		i++
+		if numerator == 0 {
+			break
+		}
 	}
 }
 
