65b080987f71f68a34b7eac86afef299bdd1d00d
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2024-09-14 11:43:05 +0100

    calculateFutures func

--- ch1/investment_calculator.go
+++ ch1/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var expectedReturnRate float64
 	var years float64
@@ -20,9 +21,28 @@ func main() {
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Printf("Future value: %v \nFuture real valuue: %v", futureValue, futureRealValue)
+	fmt.Println(futureRealValue)
+	fmt.Println("Divisor result:", divisor(90, 34))
+}
+
+func divisor(denominator float64, numerator float64) float64 {
+	if denominator == 0 {
+		return 0
+	}
+
+	return numerator / denominator
+}
+
+func calculateFutureValues(
+	investmentAmount float64, expectedReturnRate float64, years float64,
+) (float64, float64) {
+
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	return futureValue, futureRealValue
 }
